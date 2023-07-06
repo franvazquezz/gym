954d3144d13371f003066ca8b7dd025b500e69e2
franvazquezz <franvazquez2001@gmail.com>
2023-07-05 21:42:48 -0300

    error de gil

--- src/components/userpicker.jsx
+++ src/components/userpicker.jsx
@@ -15,16 +15,16 @@ const User = () => {
 
 // 	})
 	return (
-		<div>
-			<section className="bg-white">
-				<div className="mx-auto max-w-screen-xl flex-wrap px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+		<div className=" min-w-screen  min-h-screen">
+			<section className="">
+				<div className="mt-8 grid grid-cols-1 gap-8">
 					<h1 className="justify-center text-3xl flex font-bold text-indigo-600 sm:text-3xl font-[Poppins] mt-0">
 						Plan de gimansio
 					</h1>
 					<h2 className="justify-center text-xl flex font-bold text-indigo-600 sm:text-xl font-[Poppins]">
 						Prof. Luca Lopensino
 					</h2>
-					<div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2">
+					<div className="m-8 grid grid-cols-1 gap-8 lg:grid-cols-3">
 						<Link
 							to={`/home/pancho`}
 						>
